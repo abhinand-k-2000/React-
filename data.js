41a1e110c51985d42489501970a4c5c13882b4d2
Abhinand K <abhinand8413@gmail.com>
2024-04-05 10:43:53 +0530

    day3 - started project

---
+++ data.js
@@ -0,0 +1,521 @@
+ const restaurants =  [
+    {
+      "info": {
+        "id": "114246",
+        "name": "Burger Lounge- Beach Road",
+        "cloudinaryImageId": "mbldqkrpztivwwgymlsn",
+        "locality": "Silk Street",
+        "areaName": "Kuttichira",
+        "costForTwo": "₹200 for two",
+        "cuisines": [
+          "Burgers",
+          "Ice Cream"
+        ],
+        "avgRating": 4.2,
+        "parentId": "18924",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 39,
+          "lastMileTravel": 5.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "39 mins",
+          "lastMileTravelString": "5.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-04-04 23:30:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+              "description": "OnlyOnSwiggy"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "OnlyOnSwiggy",
+                    "imageId": "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹249",
+          "discountTag": "FLAT DEAL"
+        },  
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "maxDuration": "3000",
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        "context": "seo-data-b40e2357-35f2-437e-90ba-a3acf3e5acb3"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/burger-lounge-beach-road-silk-street-kuttichira-114246",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_BCP_byName"
+    },
+    {
+      "info": {
+        "id": "432035",
+        "name": "Club Sulaimani",
+        "cloudinaryImageId": "sibiokm1aoo1nm79yw5g",
+        "locality": "Beach Road",
+        "areaName": "Kuttichira",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "Beverages",
+          "Burgers",
+          "Pizzas"
+        ],
+        "avgRating": 4,
+        "parentId": "63985",
+        "avgRatingString": "4.0",
+        "totalRatingsString": "50+",
+        "sla": {
+          "deliveryTime": 39,
+          "lastMileTravel": 5.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "39 mins",
+          "lastMileTravelString": "5.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-04-04 23:44:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "aggregatedDiscountInfoV2": {
+          
+        },
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "maxDuration": "3000",
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        "context": "seo-data-b40e2357-35f2-437e-90ba-a3acf3e5acb3"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/club-sulaimani-beach-road-kuttichira-432035",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_BCP_byName"
+    },
+    {
+      "info": {
+        "id": "729144",
+        "name": "Pizza Hut",
+        "cloudinaryImageId": "978358a0d0caba4f4555b8e2147b467d",
+        "locality": "Beach Road",
+        "areaName": "Calicut",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Pizzas"
+        ],
+        "avgRating": 4.2,
+        "parentId": "721",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "100+",
+        "sla": {
+          "deliveryTime": 43,
+          "lastMileTravel": 6.2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "43 mins",
+          "lastMileTravelString": "6.2 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-04-05 04:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Pizza.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Pizza.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "maxDuration": "3000",
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        "context": "seo-data-b40e2357-35f2-437e-90ba-a3acf3e5acb3"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/pizza-hut-beach-road-calicut-729144",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_BCP_byName"
+    },
+    {
+      "info": {
+        "id": "324838",
+        "name": "Coral Bay by Sea Queen Hotel",
+        "cloudinaryImageId": "zkfdvukmfyzmw9bpe6bb",
+        "locality": "Beach Road",
+        "areaName": "Palayam",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "South Indian",
+          "North Indian",
+          "Chinese",
+          "Thai",
+          "Salads",
+          "Pastas",
+          "Tandoor",
+          "Continental",
+          "Beverages"
+        ],
+        "avgRating": 4.2,
+        "parentId": "65202",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "500+",
+        "sla": {
+          "deliveryTime": 43,
+          "lastMileTravel": 6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "43 mins",
+          "lastMileTravelString": "6.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-04-04 22:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹249",
+          "discountTag": "FLAT DEAL"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "maxDuration": "3000",
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        "context": "seo-data-b40e2357-35f2-437e-90ba-a3acf3e5acb3"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/coral-bay-by-sea-queen-hotel-beach-road-palayam-324838",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_BCP_byName"
+    },
+    {
+      "info": {
+        "id": "323700",
+        "name": "Kuttichira Biriyani Centre",
+        "cloudinaryImageId": "zmbvzmrvnjsfby2hdnx5",
+        "locality": "S Beach Road",
+        "areaName": "Kuttichira",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "Biryani",
+          "South Indian"
+        ],
+        "avgRating": 4.4,
+        "parentId": "122112",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 39,
+          "lastMileTravel": 5.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "39 mins",
+          "lastMileTravelString": "5.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-04-04 20:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Biryani.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Biryani.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "maxDuration": "3000",
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        "context": "seo-data-b40e2357-35f2-437e-90ba-a3acf3e5acb3"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/kuttichira-biriyani-centre-s-beach-road-kuttichira-323700",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_BCP_byName"
+    },
+]
